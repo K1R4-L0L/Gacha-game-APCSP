b8047784953729a1214d395109ef65406ee780c1
yadiel <yadield2889@tuckahoeschools.org>
2025-01-21 13:32:44 +0000

    intial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+let threeStar = ["trash","poop", "banana"];
+let fourstar = ["github guy", "wumpus","snapchat ghost"]
+let fivestar =["ugly dog", "spiderman", "silly guy", "war criminal", "sparkle", "magik", "bakugo"]
+let index = 0;
