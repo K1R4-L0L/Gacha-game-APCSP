5f10019908959507a2d95a5b4961dbf0bbb0e2de
yadiel <yadield2889@tuckahoeschools.org>
2025-01-27 13:30:31 +0000

    list made and functions

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 let threeStar = ["trash","poop", "banana"];
-let fourstar = ["github guy", "wumpus","snapchat ghost"]
+let fourstar = ["github guy", "wumpus","snapchat ghost", "el primo"]
 let fivestar =["ugly dog", "spiderman", "silly guy", "war criminal", "sparkle", "magik", "bakugo"]
+let sixstar = ["buster","mercuria","jessica","kafka","jane doe","isolde","skylar", "qingque","ena", "mizuki", "fang","melodie", "colette", "balloon party", "schneider"]
 let index = 0;
+let score = 0;
+const box = document.querySelector(".lootbox")
+const openbox = document.querySelector(".open-lb")
+const button = document.querySelector(".button")
+
+// function to open the loot box 
+let openingBox  = () => {
+    button.addEventListener('click', () =>{
+    for
