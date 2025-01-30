9793f7cb71bc296c6cca1e870f5ac384f8cf81c3
yadiel <yadield2889@tuckahoeschools.org>
2025-01-30 13:31:15 +0000

    idk what im doin

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let threeStar = ["trash","poop", "banana"];
-let fourstar = ["github guy", "wumpus","snapchat ghost", "el primo"]
-let fivestar =["ugly dog", "spiderman", "silly guy", "war criminal", "sparkle", "magik", "bakugo"]
-let sixstar = ["buster","mercuria","jessica","kafka","jane doe","isolde","skylar", "qingque","ena", "mizuki", "fang","melodie", "colette", "balloon party", "schneider"]
-let index = 0;
+let fourStar = ["github guy", "wumpus","snapchat ghost", "el primo"]
+let fiveStar =["ugly dog", "spiderman", "silly guy", "war criminal", "sparkle", "magik", "bakugo", "deku"]
+let sixStar = ["buster","mercuria","jessica","kafka","jane doe","isolde","skylar", "qingque","ena", "mizuki", "fang","melodie", "colette", "balloon party", "schneider"]
+let indexBook = ["trash","poop", "banana","github guy", "wumpus","snapchat ghost", "el primo","ugly dog", "spiderman", "silly guy", "war criminal", "sparkle", "magik", "bakugo", "deku", "buster","mercuria","jessica","kafka","jane doe","isolde","skylar", "qingque","ena", "mizuki", "fang","melodie", "colette", "balloon party", "schneider"]
 let score = 0;
+
 const box = document.querySelector(".lootbox")
 const openbox = document.querySelector(".open-lb")
 const button = document.querySelector(".button")
@@ -13,9 +14,12 @@ let openingBox  = () => {
     button.addEventListener('click', () =>{
         for (let i = 0; i < 9; i++) {
             if(probability(80)){
-                let randomThree = Math.floor(Math.random() * 3) + 1;
                 randomThree = threeStar
-                if "trash"
+                let randomThree = Math.floor(Math.random() * 3) + 1;
+                if(randomThree[0]  != indexbook[0]){
+                     let hasBook =+ indexBook[0];
+                    console.log(myArray[0]);
+
             }
 
           }})}
