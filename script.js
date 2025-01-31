6b3effe8583e0f19bfb732c5893155c773940e7a
yadiel <yadield2889@tuckahoeschools.org>
2025-01-31 13:31:26 +0000

    im so cooked

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let fiveStar =["ugly dog", "spiderman", "silly guy", "war criminal", "sparkle", 
 let sixStar = ["buster","mercuria","jessica","kafka","jane doe","isolde","skylar", "qingque","ena", "mizuki", "fang","melodie", "colette", "balloon party", "schneider"]
 let indexBook = ["trash","poop", "banana","github guy", "wumpus","snapchat ghost", "el primo","ugly dog", "spiderman", "silly guy", "war criminal", "sparkle", "magik", "bakugo", "deku", "buster","mercuria","jessica","kafka","jane doe","isolde","skylar", "qingque","ena", "mizuki", "fang","melodie", "colette", "balloon party", "schneider"]
 let score = 0;
-
+let hasBook = 0;
 const box = document.querySelector(".lootbox")
 const openbox = document.querySelector(".open-lb")
 const button = document.querySelector(".button")
@@ -15,10 +15,14 @@ let openingBox  = () => {
         for (let i = 0; i < 9; i++) {
             if(probability(80)){
                 randomThree = threeStar
-                let randomThree = Math.floor(Math.random() * 3) + 1;
-                if(randomThree[0]  != indexbook[0]){
-                     let hasBook =+ indexBook[0];
-                    console.log(myArray[0]);
+                let randomThree = Math.floor(Math.random() * 3) + 0;
+            
+                if(randomThree  != indexBool){
+                    hasBook += indexBook[0];
+                    console.log(indexBook)
+                }else{
+                    console.log(hasBook)
+                }
 
             }
 
